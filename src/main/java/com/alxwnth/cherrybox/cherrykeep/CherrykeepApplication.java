5e1ca157d0fb7f85551202435f0f2ac46be3c1d6
nxvembr <nxvembr@posteo.me>
2024-05-09 19:11:09 +0300

    chore: Spring project init

---
+++ src/main/java/com/alxwnth/cherrybox/cherrykeep/CherrykeepApplication.java
@@ -0,0 +1,13 @@
+package com.alxwnth.cherrybox.cherrykeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CherrykeepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CherrykeepApplication.class, args);
+	}
+
+}
