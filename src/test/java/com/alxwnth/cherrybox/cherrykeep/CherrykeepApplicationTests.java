b913f8f4ad60f08d81786003edf1c1642cd568e5
Alex <alxwnth@posteo.me>
2024-05-09 19:11:09 +0300

    chore: Spring project init

---
+++ src/test/java/com/alxwnth/cherrybox/cherrykeep/CherrykeepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alxwnth.cherrybox.cherrykeep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CherrykeepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
