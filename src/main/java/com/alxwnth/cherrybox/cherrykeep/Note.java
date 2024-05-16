05c041aeda737be7a942d4f919a0d7f6603ca2fb
nxvembr <nxvembr@posteo.me>
2024-05-16 18:59:24 +0300

    feat: Create Note class

---
+++ src/main/java/com/alxwnth/cherrybox/cherrykeep/Note.java
@@ -0,0 +1,43 @@
+package com.alxwnth.cherrybox.cherrykeep;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+class Note {
+    private @Id
+    @GeneratedValue Long id;
+    private String text;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date expiresAt;
+
+    public Note() {}
+
+    public Note(String text, Date expiresAt) {
+        this.text = text;
+        this.expiresAt = expiresAt;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public Date getExpiresAt() {
+        return expiresAt;
+    }
+
+    public void setExpiresAt(Date expiresAt) {
+        this.expiresAt = expiresAt;
+    }
+}
