fac3bd7791d428988d7a37a47f1094afa64c3fef
Alex <alxwnth@posteo.me>
2024-05-21 18:58:59 +0300

    feat: Handle login

--- src/main/java/com/alxwnth/cherrybox/cherrykeep/config/WebSecurityConfig.java
+++ src/main/java/com/alxwnth/cherrybox/cherrykeep/config/WebSecurityConfig.java
@@ -1,19 +1,27 @@
 package com.alxwnth.cherrybox.cherrykeep.config;
 
+import com.alxwnth.cherrybox.cherrykeep.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
+    private final UserService userService;
+    private final Argon2PasswordEncoder argon2PasswordEncoder;
+
+    public WebSecurityConfig(UserService userService, Argon2PasswordEncoder argon2PasswordEncoder) {
+        this.userService = userService;
+        this.argon2PasswordEncoder = argon2PasswordEncoder;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -28,15 +36,8 @@ public class WebSecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
+    @Autowired
+    protected void configureGlobals(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(argon2PasswordEncoder);
     }
 }
