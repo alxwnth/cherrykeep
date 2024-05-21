89affa495969440f2725df4d1230ccf43df73b4a
Alex <alxwnth@posteo.me>
2024-05-21 18:49:07 +0300

    feat: Implement sign up

--- src/main/java/com/alxwnth/cherrybox/cherrykeep/config/WebSecurityConfig.java
+++ src/main/java/com/alxwnth/cherrybox/cherrykeep/config/WebSecurityConfig.java
@@ -19,7 +19,8 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/home").permitAll()
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/signup").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form.loginPage("/login").permitAll())
