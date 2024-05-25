e18447ba01ca7810223986368bec6c7e04c5eda0
Alex <alxwnth@posteo.me>
2024-05-25 16:21:54 +0300

    fix: Import styles for processing

--- src/main/java/com/alxwnth/cherrybox/cherrykeep/config/WebSecurityConfig.java
+++ src/main/java/com/alxwnth/cherrybox/cherrykeep/config/WebSecurityConfig.java
@@ -27,8 +27,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/").permitAll()
-                        .requestMatchers("/signup").permitAll()
+                        .requestMatchers("/", "/signup", "/js/**", "/css/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form.loginPage("/login").permitAll())
